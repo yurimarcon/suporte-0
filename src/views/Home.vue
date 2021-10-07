9eefe15360b75d72f3effd54c077b27b5b70a1c7
yurimarcon <yuri.erik.oliveira@gmail.com>
2021-10-07 09:48:54 -0300

    testes de CRUD na Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,61 @@
 <template>
-  <hello-world />
+  <div>
+    <v-btn
+    @click="gravaDocumento()"
+    color="primary"
+    block
+    >
+      Buscar
+    </v-btn>
+    
+    <v-btn
+    @click="removerDocumento()"
+    color="error"
+    block
+    >
+      Remove
+    </v-btn>
+    {{retorno}}
+  </div>
+
+  
+
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+import '../services/firebase'
+import { getDatabase, onValue, ref, set, remove } from "firebase/database";
+const db = getDatabase();
 
-  export default {
-    name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
-    created(){
-      this.$store.dispatch('verifyAuth');
+export default {
+  data:()=>{
+    return{
+      retorno: null
     }
+  },
+  methods:{
+    buscaRegistros(){
+      onValue(ref(db, 'Usuarios/'), snapshot => {
+        console.log(snapshot)
+        this.retorno = snapshot.val();
+      });
+    },
+    gravaDocumento(){
+      let id = new Date().getTime();
+      set(ref(db, 'Usuarios/' + id), {
+      // set(ref(db, 'Usuarios/' + 1633609746505), {
+        username: "Yuri",
+        email: "yuri@email",
+        id: id
+      });
+    },
+    removerDocumento(){
+      remove(ref(db, 'Usuarios/' + 1633609728356))
+    }
+  },
+  created(){
+    this.$store.dispatch('verifyAuth');
+    this.buscaRegistros();
   }
+}
 </script>

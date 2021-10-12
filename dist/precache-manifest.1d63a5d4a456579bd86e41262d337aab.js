0bd68a409deb5d253ca6a27c0ef2e68860383740
yurimarcon <yuri.erik.oliveira@gmail.com>
2021-10-11 22:58:27 -0300

    Criação de tela e métodos para criação de chamado na base

---
+++ dist/precache-manifest.1d63a5d4a456579bd86e41262d337aab.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e64f5afae0a890d63071",
+    "url": "/css/app.a7e36c46.css"
+  },
+  {
+    "revision": "acf880c3a2308c746c37",
+    "url": "/css/chunk-vendors.1ed1e5a4.css"
+  },
+  {
+    "revision": "4ceb6da5017515a2500cd288984ef2ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e64f5afae0a890d63071",
+    "url": "/js/app.5df8be96.js"
+  },
+  {
+    "revision": "9bf524bf87412ff1feb8",
+    "url": "/js/chunk-2d2086b7.5e8100e0.js"
+  },
+  {
+    "revision": "fb29e65533189768c2e7",
+    "url": "/js/chunk-2d21eadd.eb7b000a.js"
+  },
+  {
+    "revision": "acf880c3a2308c746c37",
+    "url": "/js/chunk-vendors.5a7f7e45.js"
+  },
+  {
+    "revision": "c18b1067b34bce4ea54b2f807a396f84",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
